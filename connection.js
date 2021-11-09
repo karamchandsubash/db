94b4ad68b9f51f6ba0f020e3442df22e779f873c
KARAMCHAND SUBASH K <karamchandsubash@gmail.com>
2021-11-09 11:36:45 +0530

    Update connection.js

--- connection.js
+++ connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const ConnectDB = async ()=>{
-    await mongoose.connect("mongodb+srv://ashok:ashok@cluster0.miwvn.mongodb.net/express?retryWrites=true&w=majority");
+    await mongoose.connect("mongodb+srv://Subash_11:Subash29@cluster0.o5gqm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
 }
 
 module.exports = ConnectDB;
